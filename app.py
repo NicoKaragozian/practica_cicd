687fdc970700ca2c1072bb170ecf0737966bdb14
Nicolas Karagozian <nkaragozian@udesa.edu.ar>
2025-10-05 12:40:20 -0300

    correccion error

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ def add(a, b):
     return a + b
 
 def subtract(a, b):
-    return a + b   # <--- ERROR INTENCIONAL
+    return a - b   # <--- ERROR INTENCIONAL
 
 def multiply(a, b):
     return a * b
